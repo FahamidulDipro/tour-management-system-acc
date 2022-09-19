1319a182ccd14d50adb7932d59a9c8bf629a99c3
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-09-19 17:42:28 +0600

    server updated

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-// const DBConnect = require("./utils/dbConnect");
 const mongoose = require("mongoose");
 
 
 const app = require("./app");
 
-// database connection
-// DBConnect();
+
 const port = process.env.PORT || 8080;
 mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
   console.log(`Database connection is successfull!` .yellow.bold)
--- utils/dbConnect.js
+++
@@ -1,6 +0,0 @@
-
-
-const port = process.env.PORT || 8080;
-mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
-  console.log("Database connection is successfull!")
-})
